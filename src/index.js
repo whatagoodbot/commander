4c1dc197682e6bc28340a0f3dbf6a67fc45ca35b
notABluesSinger <rob@whatagoodbot.com>
2022-11-16 10:02:54 +0000

    missed a const

--- src/index.js
+++ src/index.js
@@ -51,6 +51,7 @@ broker.client.on('message', async (topic, data) => {
       const processedResponses = await searchForCommand(validatedRequest, repeaters)
       if (!processedResponses || !processedResponses.length) return
       for (const current in processedResponses) {  
+        const processedResponse = processedResponses[current]
         const validatedResponse = broker[processedResponse.topic].validate({
           ...validatedRequest,
           ...processedResponse.payload
